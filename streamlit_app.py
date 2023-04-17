1fc113e9bfea52021b560069330a48e069d3b5f6
raymond-major-410 <65994021+raymond-major-410@users.noreply.github.com>
2023-04-17 01:21:45 -0400

    Update streamlit_app.py
    added streamlit.stop() to hold a section of the app while i troubleshoot

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -33,6 +34,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #output to screen a table format 
 streamlit.dataframe(fruityvice_normalized)
 
+streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
